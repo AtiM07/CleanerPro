dfd90175289820ed79e473970955291d6e21b858
AtiM07 <atim203011@gmail.com>
2021-07-28 14:02:29 +0300

    updated move

--- CleanerPro/Assets/Scripts/Person.cs
+++ CleanerPro/Assets/Scripts/Person.cs
@@ -28,104 +28,6 @@ public class Person : MonoBehaviour
     private Vector2Int currentPos;
 
     /// <summary>
-    /// ƒвижение персонажа в определенном направлении
-    /// </summary>
-    /// <param name="direction">направление движени€</param>
-    public void Move(Vector2Int direction)
-    {
-        currentPos = new Vector2Int(Instance.X, Instance.Y);
-        StartCoroutine(DirectionCoroutine(direction));
-    }
-
-    IEnumerator DirectionCoroutine(Vector2Int direction)
-    {
-        isTravelling = true;
-        Vector2Int nextPos = currentPos + direction;
-
-        Cell nextCell = Field.Instance.field[0, 0];
-
-        for (int x = 0; x < Field.Instance.FieldSize; x++)
-        {
-            for (int y = 0; y < Field.Instance.FieldSize; y++)
-            {
-                if (Field.Instance.field[x, y].X == nextPos.x && Field.Instance.field[x, y].Y == nextPos.y)
-                    nextCell = Field.Instance.field[nextPos.x, nextPos.y];
-            }
-        }
-
-        if (nextCell.Type == Cell.CellType.Wall)
-        {
-            isTravelling = false;
-            yield break;
-        }
-
-        while (nextCell.Type != Cell.CellType.Wall &&
-            (nextPos.x > -1 && nextPos.y > -1 && nextPos.x < Field.Instance.FieldSize && nextPos.y < Field.Instance.FieldSize))
-        {
-
-            yield return Clear();
-            //yield return MoveCoroutine(currentPos, nextPos, direction);
-            //if (nextPos.x < Field.Instance.FieldSize && nextPos.y < Field.Instance.FieldSize)
-            //{           
-                Instance.transform.position = (Vector2)Field.Instance.field[nextPos.x, nextPos.y].transform.position;
-                Instance.X = Field.Instance.field[nextPos.x, nextPos.y].X;
-                Instance.Y = Field.Instance.field[nextPos.x, nextPos.y].Y;
-            
-            //}
-
-            currentPos += direction;
-            nextPos = currentPos + direction;
-
-            for (int x = 0; x < Field.Instance.FieldSize; x++)
-            {
-                for (int y = 0; y < Field.Instance.FieldSize; y++)
-                {
-                    if (Field.Instance.field[x, y].X == nextPos.x && Field.Instance.field[x, y].Y == nextPos.y)
-                        nextCell = Field.Instance.field[nextPos.x, nextPos.y];
-                }
-            }
-        }
-
-        isTravelling = false;
-    }
-
-    //private IEnumerator MoveCoroutine(Vector2Int startPos, Vector2Int endPos, Vector2Int direction)
-    //{
-    //    Vector2 currentPlayerPos = startPos;
-
-    //    while (Vector2.Distance(currentPlayerPos, endPos) < 0.01f)
-    //    {
-    //        Instance.transform.position = currentPlayerPos;
-    //        currentPlayerPos += Time.deltaTime * 2f * (Vector2)direction;
-    //    }
-
-    //    SetValue(endPos.x, endPos.y);
-
-    //    yield break;
-    //}
-
-    /// <summary>
-    /// ќчищение €чеек, наход€щихс€ под персонажем
-    /// </summary>
-    /// <returns></returns>
-    IEnumerator Clear()
-    {
-        for (int x = 0; x < Field.Instance.FieldSize; x++)
-        {
-            for (int y = 0; y < Field.Instance.FieldSize; y++)
-            {
-                if (Instance.transform.position == Field.Instance.field[x, y].transform.position)
-                {
-                    Field.Instance.field[x, y].UpdateType(Cell.CellType.None);
-                }
-
-            }
-        }
-
-        yield return ClearCell();
-    }
-
-    /// <summary>
     /// ѕроверка, все ли €чейки на поле очищены
     /// </summary>
     /// <returns></returns>
@@ -144,11 +46,92 @@ public class Person : MonoBehaviour
         if (trashCell == 0 && !isWinner)
         {
             isWinner = true;
-            yield return new WaitForSeconds(0.05f);
+            yield return new WaitForSeconds(0.25f);
             GameController.Instance.NextLvl();
             yield break;
         }
     }
 
+
+    private void Update()
+    {
+        Clear2();
+    }
+
+    public void Move(Vector2 direction)
+    {
+
+        int x=0, y=0;
+        if (direction == Vector2.right)
+            x = 1;
+        else
+        if (direction == Vector2.left)
+            x = -1;
+        else
+        if (direction == Vector2.up)
+            y = 1;
+        else
+        if (direction == Vector2.down)
+            y = -1;
+        StartCoroutine(MoveCoroutine(x, y));
+    }
+
+    IEnumerator MoveCoroutine(int dirX, int dirY)
+    {
+
+        if (isTravelling)
+            yield break;
+
+        isTravelling = true;
+
+        if (dirY == 0)
+        {
+            for (int X = Instance.X + dirX; X < Field.Instance.FieldSize && X > -1; X += dirX)
+            {
+                Cell nextCell = Field.Instance.field[X, Y];
+                if (nextCell.Type != Cell.CellType.Wall)
+                {
+                    Instance.SetValue(X, Y);
+                    Instance.transform.position = nextCell.transform.position;
+                    Clear2();
+                }
+                else
+                {
+                    isTravelling = false;
+                    yield break;
+                }
+
+            }
+        }
+
+        if (dirX == 0)
+        {
+            for (int Y = Instance.Y + dirY; Y < Field.Instance.FieldSize && Y > -1; Y += dirY)
+            {
+                Cell nextCell = Field.Instance.field[X, Y];
+                if (nextCell.Type != Cell.CellType.Wall)
+                {
+                    Instance.SetValue(X, Y);
+                    Instance.transform.position = nextCell.transform.position;
+                    Clear2();
+                }
+                else
+                {
+                    isTravelling = false;
+                    yield break;
+                }
+
+            }
+        }
+
+        isTravelling = false;
+    }
+
+    void Clear2()
+    {
+        if (Field.Instance.field[X, Y].Type == Cell.CellType.Trash)
+            Field.Instance.field[X, Y].UpdateType(Cell.CellType.None);
+        StartCoroutine(ClearCell());
+    }
 
 }
